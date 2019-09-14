a55d6e5c5f4379d74bcab325a32c49f30090fab3
Guilherme Augusto Alves <41652807+alvesmog@users.noreply.github.com>
2019-09-14 13:23:22 -0300

    atributos privados

--- unit-spw-2-2.1/src/aula3/Disciplina.java
+++ unit-spw-2-2.1/src/aula3/Disciplina.java
@@ -4,5 +4,10 @@ import java.util.ArrayList;
 
 public class Disciplina {
 	String nome;
-	ArrayList<Aluno> alunos = new ArrayList<Aluno>();
+	private ArrayList<Aluno> alunos = new ArrayList<Aluno>();
+	int QTD_MAX_ALUNOS = 30;
+	
+	void adicionarAluno(Aluno matricula) {
+		alunos.add(matricula);
+	}
 }
